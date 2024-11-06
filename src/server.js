c8bf1c47051e95fbcce8822c665021321cf14a97
João Gabriel Salvador Ito <38169465+Gabuito@users.noreply.github.com>
2024-11-06 18:39:56 -0300

    fix server file

--- src/server.js
+++ src/server.js
@@ -37,6 +37,6 @@ app.get('/login2', (req, res) => {
   res.status(200).render('dash/login.ejs', {layout : 'dash/layout/layout.ejs', perfil : {}});
 });
 
-app.listen( process.env.PORT || 5000, '0.0.0.0', () => {
-  console.log('\n Server está ativo -> http://localhost:5000 \n');
+app.listen(PORT || 5000, '0.0.0.0', () => {
+  console.log('\n Server está ativo -> http://localhost:${PORT} \n');
 });
